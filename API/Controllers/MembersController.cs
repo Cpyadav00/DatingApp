024894d06bcc3574088d0de774d9c360663af11c
Chandra Prakash Yadav <hcpyadav2509@gmail.com>
2025-07-29 21:33:55 +0530

    Section 4 is completed

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,13 +1,12 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -15,6 +14,7 @@ namespace API.Controllers
             var member =await context.Users.ToListAsync();
             return member;
         }
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
